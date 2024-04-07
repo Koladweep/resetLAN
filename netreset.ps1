f8a1968cdcff42a03481deec4b7bf9a6244e53df
Nayan Chandwani <102764937+Koladweep@users.noreply.github.com>
2024-04-07 18:32:07 +0530

    Update netreset.ps1

--- netreset.ps1
+++ netreset.ps1
@@ -1,1 +1,3 @@
 # no more scripts!
+# anyone could replace this script with a malicious code and that could lead to it getting executed with admin priviliges.
+# BTW our task is apparantly too simple to warrant a script!
